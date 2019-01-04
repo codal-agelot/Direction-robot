2e8703004f0d19bc5d433819a5809c5de8412121
Abhishek Gelot <46366650+codal-agelot@users.noreply.github.com>
2019-01-04 14:48:36 +0530

    To cross check the position

--- code.py
+++ code.py
@@ -66,3 +66,9 @@ for i in path:
 
 print("final position,{}".format(final_position))
 print("original position, {}".format(position))
+
+# if position == To_check[-1]:
+#     print("true")
+# else:
+#     print("false")
+
